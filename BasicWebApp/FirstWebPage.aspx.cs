6d71b7217504db674712846734ad9f2940ed6fb3
Edin Jašarević <edinjasar14@gmail.com>
2018-07-28 13:45:30 +0200

    dogadjaj na dugmetu

--- BasicWebApp/FirstWebPage.aspx.cs
+++ BasicWebApp/FirstWebPage.aspx.cs
@@ -16,7 +16,7 @@ namespace BasicWebApp
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-
+            Label1.Text = TextBox1.Text + ", welcome to Visual Studio!";
         }
     }
 }
